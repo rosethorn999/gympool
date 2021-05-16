cabdd60efc08e99ae758d569a0b9410a63a91930
rosethorn999 <24700793+rosethorn999@users.noreply.github.com>
2021-05-17 01:08:10 +0800

    Add script to build difference env

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,19 +1,6 @@
 import axios from "axios";
 
-let host = "";
-switch (process.env.NODE_ENV) {
-  case "development":
-  case "debug":
-    host = "http://150.117.81.50:8000/api";
-    break;
-  case "production":
-    host = "https://gympool-test.herokuapp.com/api";
-    break;
-  default:
-    console.warn("set axios host error");
-    break;
-}
-
+let host = process.env.REACT_APP_HOST;
 const basicRequest = axios.create({
   baseURL: host,
 });
