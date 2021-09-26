7f92f1702ed48572d25e856e297456ca71c1a325
rosethorn999 <24700793+rosethorn999@users.noreply.github.com>
2021-09-26 23:58:21 +0800

    Add icons& input focus style

--- src/pages/Index/components/MobileNav.js
+++ src/pages/Index/components/MobileNav.js
@@ -1,28 +1,50 @@
 import "../scss/MobileNav.scss";
-import { HashRouter as Router, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { HashRouter as Router, Link, useLocation } from "react-router-dom";
+import homeIcon from "../assets/home.png";
+import memberIcon from "../assets/member.png";
+import searchIcon from "../assets/search.png";
 
 function MobileNav() {
+  const location = useLocation();
+  const [activeTab, setActiveTab] = useState(location.pathname);
+  useEffect(() => {
+    setActiveTab(location.pathname);
+  }, [location]);
+
   return (
     <Router>
       <div className="MobileNav">
         <div className="flex-container">
           <div>
-            <Link to="/">
-              <img src="https://via.placeholder.com/50" alt="home" />
+            <Link
+              to="/"
+              className={`${activeTab === "/" ? "active" : ""}`}
+              onClick={() => setActiveTab("/")}
+            >
+              <img src={homeIcon} alt="home" />
               <br />
               首頁
             </Link>
           </div>
           <div>
-            <Link to="/record">
-              <img src="https://via.placeholder.com/50" alt="search" />
+            <Link
+              to="/record"
+              className={`${activeTab === "/record" ? "active" : ""}`}
+              onClick={() => setActiveTab("/record")}
+            >
+              <img src={searchIcon} alt="search" />
               <br />
               搜尋
             </Link>
           </div>
           <div>
-            <Link to="/login">
-              <img src="https://via.placeholder.com/50" alt="member" />
+            <Link
+              to="/login"
+              className={`${activeTab === "/login" ? "active" : ""}`}
+              onClick={() => setActiveTab("/login")}
+            >
+              <img src={memberIcon} alt="member" />
               <br />
               會員
             </Link>
